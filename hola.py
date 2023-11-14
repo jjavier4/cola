b92f155fb53f05827934da52731a203b51f4df4d
javier <javierpalmaaguilar117@gmail.com>
2023-11-14 09:32:50 -0600

    comit echo ya en github

--- hola.py
+++ hola.py
@@ -2,3 +2,9 @@ print(2**2)
 print("estos son los cambios adicionales")
 print("no se si funcione esto")
 print("esto nueva liena es para confirmar el reset")
+
+
+x = [1,2,3,4,5]
+
+for i in x:
+    print(i)
